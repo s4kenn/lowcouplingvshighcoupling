b7f18975d5e2526ec130de4a7136a1b44369ecc0
Aditya Singh <yeezusgod01@gmail.com>
2024-12-02 11:39:44 +0530

    Tight vs Loose Coupling

--- src/main/java/com/loose/coupling/LooseCouplingExample.java
+++ src/main/java/com/loose/coupling/LooseCouplingExample.java
@@ -2,7 +2,7 @@ package com.loose.coupling;
 
 public class LooseCouplingExample {
     public static void main(String[] args) {
-        MongoDBProvider mongoDBProvider = new MongoDBProvider();
+        UserDataProvider mongoDBProvider = new MongoDBProvider();
         UserManager userManager = new UserManager(mongoDBProvider);
         System.out.println(userManager.getUserInfo());
     }
